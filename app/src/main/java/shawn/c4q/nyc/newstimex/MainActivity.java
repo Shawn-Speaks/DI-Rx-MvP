a0726ed2be3dc8e5dc14d4ab0a4462e93bf3d3b0
shawn-speaks <sspeaks121@gmail.com>
2017-05-11 13:25:39 -0400

    initial commit

---
+++ app/src/main/java/shawn/c4q/nyc/newstimex/MainActivity.java
@@ -0,0 +1,30 @@
+package shawn.c4q.nyc.newstimex;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView mTextView;
+    @BindView(R.id.button)
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button) void clicked(){
+        int number = Integer.parseInt(mTextView.getText().toString());
+        mTextView.setText(String.valueOf(number+1));
+    }
+}
